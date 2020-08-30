9b49038142c44ccd026d8b31163d8ffd8ef911cb
Christopher Hoover <ch@murgatroid.com>
2020-08-30 13:15:31 -0700

    Reformat & chmod

--- bin/fnlk.sh
+++ bin/fnlk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for fn_lock in /sys/bus/hid/devices/*/fn_lock; do
-  case $(cat "${fn_lock}") in
+    case $(cat "${fn_lock}") in
 	0)
-    echo -n 1 >"${fn_lock}"
-	  ;;
+	    echo -n 1 >"${fn_lock}"
+	    ;;
 	1)
-	  echo -n 0 >"${fn_lock}"
-	  ;;
-  esac
+	    echo -n 0 >"${fn_lock}"
+	    ;;
+    esac
 done
