d967e3e5a9d57cc44be01a78675322bf30a46535
Christopher Hoover <ch@murgatroid.com>
2024-05-29 10:40:41 -0700

    .

--- gps/gps_antenna_plus.sh
+++ gps/gps_antenna_plus.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo modprobe cp210x
-echo -n 10c4 8116 | sudo tee /sys/bus/usb-serial/drivers/cp210x/new_id
+modprobe cp210x
+echo -n 10c4 8116 >/sys/bus/usb-serial/drivers/cp210x/new_id
