af6c21bf4debf20ccc595c32d20754cba21a4669
Christopher Hoover <ch@murgatroid.com>
2025-11-22 17:35:03 -0800

    adds nrf-ble-sniffer.sh

---
+++ bin/nrf-ble-sniffer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
+
+rm -fr "${HOME}/.nrfutil" 2>/dev/null
+
+curl 'https://files.nordicsemi.com/artifactory/swtools/external/nrfutil/executables/x86_64-unknown-linux-gnu/nrfutil' -o /tmp/nrfutil 
+chmod +x /tmp/nrfutil
+/tmp/nrfutil
+
+PATH=$HOME/.nrfutil/bin:$PATH
+nrfutil install device
+nrfutil install ble-sniffer
+nrfutil device list
+#nrfutil device program --traits nordicUsb --firmware "${HOME}/.nrfutil/share/nrfutil-ble-sniffer/firmware/sniffer_nrf52840dongle_nrf52840_4.1.1.zip"
+nrfutil ble-sniffer bootstrap
+nrfutil install completion
+nrfutil completion install bash
