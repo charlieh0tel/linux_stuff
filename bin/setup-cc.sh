9ef6d855015227387dd60b3b0d23202170197e06
Christopher Hoover <ch@murgatroid.com>
2025-11-29 09:30:51 -0800

    .

--- bin/setup-cc.sh
+++ bin/setup-cc.sh
@@ -9,11 +9,9 @@ PATH="${NPM_PACKAGES}/bin:${PATH}"
 
 npm install -g @anthropic-ai/claude-code
 
-
-claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer $(<~/.github_pat)"
+if [ -r .github_pat ]; then
+    claude mcp add --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer $(<~/.github_pat)"
+fi
 
 #claude mcp add --transport sse linear https://mcp.linear.app/sse
 #claude mcp add --transport http figma-dev-mode-mcp-server http://127.0.0.1:3845/mcp
-
-
-
