fb28639b55e588b0fe13b8c9989209bcfb7be86a
Christopher Hoover <ch@murgatroid.com>
2026-04-28 10:27:59 -0700

    Rewrite hp-gps-status.pl without Expect/tied filehandle
    Device::SerialPort (tied to *FH) and Expect (which reblesses \*FH and
    stashes state in the glob's hash slot) were co-owning one filehandle.
    The last several commits were each silencing a different warning that
    this shared ownership produced during cleanup/global destruction.
    
    Drop Expect entirely and use Device::SerialPort's native read/write in
    a small state-machine loop. $port becomes the sole owner; cleanup is
    just endwin + undef $port.

--- tm/hp-gps-status.pl
+++ tm/hp-gps-status.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 use strict;
 use warnings;
@@ -6,7 +6,6 @@ use warnings;
 use Getopt::Std;
 use Device::SerialPort;
 use Curses;
-use Expect;
 
 my $opt_string = 'hp:z';
 
@@ -28,101 +27,83 @@ usage() and exit if $opt{h};
 usage() and exit if ! defined $opt{p};
 
 my $portdev = $opt{p};
-my $quiet = "1";
+my $quiet = 1;
 
-my $port = tie(*FH, 'Device::SerialPort', $portdev, $quiet)
+my $port = Device::SerialPort->new($portdev, $quiet)
     || die "Can't open serial port $portdev!\n";
 if ($opt{z}) {
     $port->baudrate(19200)	 || die "fail setting baud after 0";
     $port->parity("odd")	 || die "fail setting parity";
-    $port->databits(7)	 || die "fail setting databits";
+    $port->databits(7)		 || die "fail setting databits";
 } else {
     # The default is 9600 on 58503B.
     $port->baudrate(19200)	 || die "fail setting baud after 0";
     $port->parity("none")	 || die "fail setting parity";
-    $port->databits(8)	 || die "fail setting databits";
+    $port->databits(8)		 || die "fail setting databits";
 }
 $port->stopbits(1)	 || die "fail setting stopbits";
 $port->handshake("none") || die "fail setting handshake";
 $port->write_settings    || die "no settings";
 
-my $timeout = 5;
-my $status = 0;
+# Non-blocking-ish reads: each read() call waits at most 100ms.
+$port->read_const_time(100);
+$port->read_char_time(0);
 
-my $exp = Expect->exp_init(\*FH);
-$exp->send("*CLS\n");
-
+my $timeout_ms = 5000;
 my $win = new Curses;
 
 sub cleanup {
     endwin;
-    if (defined $exp) {
-	# hard_close, not soft_close: soft_close blocks 15s waiting
-	# for an EOF that a serial port will never send.
-	$exp->hard_close();
-	$exp = undef;
-	# exp_init did `bless \*FH, 'Expect'`, so during global
-	# destruction Expect::DESTROY would fire on *FH and call
-	# close() on the already-closed fd. Rebless into a class
-	# with no DESTROY to suppress that.
-	bless \*FH, 'main';
-    }
-    # $port holds an inner reference to the tied object; drop it
-    # so untie doesn't warn about "inner references still exist".
-    $port = undef;
-    if (tied *FH) {
-	untie *FH;
-    }
+    undef $port;
 }
 
-END {
-    cleanup;
-};
+END { cleanup }
 
-$SIG{INT}  = $SIG{TERM} = sub {
-    # If cleanup stalls, a second ^C hard-kills.
+$SIG{INT} = $SIG{TERM} = sub {
     $SIG{INT} = $SIG{TERM} = 'DEFAULT';
     cleanup;
     exit 1;
 };
 
-$exp->expect($timeout,
-	     [
-	      qr'E-[0-9][0-9][0-9]> $',
-	      sub {
-		  my $fh = shift;
-		  $fh->send("*CLS\n");
-		  $status = 0;
-		  exp_continue;
-	      }
-	     ],
-	     [
-	      qr'scpi > $',
-	      sub {
-		  my $fh = shift;
-		  if ($status) {
-		      my @lines = split(/\n/, $fh->before());
-		      shift @lines;
-		      my $status_screen = join("\n", @lines);
-		      $status_screen =~ s/\r//g;
-		      $win->clear;
-		      $win->addstr(0, 0, $status_screen);
-		      $win->refresh;
-		  }
-		  $fh->send(":SYSTEM:STATUS?\n");
-		  $status = 1;
-		  exp_continue;
-	      }
-	     ],
-	     [
-	      timeout =>
-	      sub {
-		  my $fh = shift;
-		  $fh->send("*CLS\n");
-		  $status = 0;
-		  exp_continue;
-	      }
-	     ],
-    );
+# Drain any stale input, then prime with *CLS.
+$port->read(4096);
+$port->write("*CLS\n");
 
-exit 0;
+my $buf = '';
+my $elapsed = 0;
+my $expecting_status = 0;
+
+while (1) {
+    my ($n, $chunk) = $port->read(256);
+    if ($n) {
+	$buf .= $chunk;
+	$elapsed = 0;
+    } else {
+	$elapsed += 100;
+    }
+
+    if ($buf =~ /E-\d{3}> \z/) {
+	$port->write("*CLS\n");
+	$buf = '';
+	$expecting_status = 0;
+    } elsif ($buf =~ /scpi > \z/) {
+	if ($expecting_status) {
+	    my @lines = split(/\n/, $buf);
+	    pop @lines;		# the prompt line
+	    shift @lines;	# echoed command
+	    my $screen = join("\n", @lines);
+	    $screen =~ s/\r//g;
+	    $win->clear;
+	    $win->addstr(0, 0, $screen);
+	    $win->refresh;
+	}
+	$port->write(":SYSTEM:STATUS?\n");
+	$buf = '';
+	$expecting_status = 1;
+    } elsif ($elapsed >= $timeout_ms) {
+	$port->write("*CLS\n");
+	$buf = '';
+	$expecting_status = 0;
+	$elapsed = 0;
+    }
+}
